b12e508a1b545621173726da04fbd2db51a9c05b
Sabine Maennel <sabine.maennel@gmail.com>
2015-05-01 17:39:59 +0200

    classroom menu
    soting of lessons corrected

--- fabfile.py
+++ fabfile.py
@@ -17,19 +17,20 @@ def gitlist():
 def gitclean():
     """
     stops files in gitignore from being tracked without deleting them
-    :return: git-tracked-file.txt
     """
     local('git rm --cached `git ls-files -i -X .gitignore`')
 
-def git_tryclean():
+def git_listclean():
     """
-    stops files in gitignore from being tracked without deleting them
-    :return: writes on console all files in gitignore that are still tracked
+    determines whether there are files in gitignore that are still being tracked
     """
     local('git ls-files -i -X .gitignore')
 
 
 def stage():
+    """
+    employs mentoki repo to mentoki_test
+    """
     code_dir = '/srv/http/web0263/mentoki_test/mentoki'
     with cd(code_dir):
         run("git pull")
@@ -37,28 +38,28 @@ def stage():
 
 
 def deploy():
+    """
+    employs mentoki repo to mentoki_live
+    """
     code_dir = '/srv/http/web0263/mentoki_live/mentoki'
     with cd(code_dir):
         run("git pull")
         run("touch app.wsgi")
 
-def stage():
-    code_dir = '/srv/http/web0263/mentoki_test/mentoki'
-    with cd(code_dir):
-        run("git pull")
-        run("touch app.wsgi")
-
-
 
 def prepare_commit():
+    """
+    copies production settings onto settings
+    """
     local('cp mentoki/settings_production.py mentoki/settings.py')
 
 def prepare_dev():
+    """
+    copies dev settings onto settings
+    """
     local('cp mentoki/settings_test.py mentoki/settings.py')
 
 
-def hello(name="world"):
-    print("Hello %s!" % name)
 
 
 
