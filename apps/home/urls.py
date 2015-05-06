a9aed69521898d393b61e88c1e446d763c784e5c
Sabine Maennel <sabine.maennel@gmail.com>
2015-05-06 16:09:41 +0200

    webinarseite optimiert

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns("",
     url(r'^starterkurs/(?P<tab>[a-z0-9_-]{3,50})$', StarterkursPageView.as_view(), name='starterkurs'),
     url(r'^team$', TeamPageView.as_view(), name='team'),
     url(r'^impressum$', ImpressumPageView.as_view(), name='impressum'),
-    url(r'^aktuelles$', NewsPageView.as_view(), name='news'),
+    url(r'^webinar-online-unterrichten$', NewsPageView.as_view(), name='news'),
 )
