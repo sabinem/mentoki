969ff3392ca785c8b9b7b83ea86807d746ba2616
Sabine Maennel <sabine.maennel@gmail.com>
2015-05-21 23:17:05 +0200

    contact form corrected

--- apps/contact/views_contact.py
+++ apps/contact/views_contact.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from .forms_contact import  ContactForm
 from django.views.generic import FormView, TemplateView
 from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
 
 class ContactView(FormView):
@@ -14,6 +15,10 @@ class ContactView(FormView):
         # It should return an HttpResponse.
         form.send_email_visitor()
         form.send_email_self()
+        name = form.cleaned_data['name']
+        message = mark_safe("""<h1>Hallo %s! </h1><p>Danke für Dein Interesse an Mentoki.</p><p>
+                  Du hörst von uns innerhalb der nächsten 48 Stunden.</p>""") % name
+        self.messages.info(message)
         return super(ContactView, self).form_valid(form)
 
     def get_success_url(self):
