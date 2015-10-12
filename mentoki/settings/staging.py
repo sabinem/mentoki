08608fe57a66e47e6fc8235996618a37004915a4
Sabine Maennel <sabine.maennel@gmail.com>
2015-10-12 23:55:05 +0200

    staging settings corrected

--- mentoki/settings/staging.py
+++ mentoki/settings/staging.py
@@ -7,6 +7,10 @@ Entry point for the staging site.
 from __future__ import absolute_import, unicode_literals
 from .base import *
 
+
+# ssl settings
+SSLIFY_DISABLE = True
+
 # static and media root
 STATIC_ROOT = os.path.join(BASE_DIR, 'htdocs', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'htdocs', 'media')
