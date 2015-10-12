e89dfcd5326c035cbb2df9c7dd89777f3125e913
Sabine Maennel <sabine.maennel@gmail.com>
2015-10-13 00:06:08 +0200

    overwrite of midlleware for staging to get rid os sslify

--- mentoki/settings/production.py
+++ mentoki/settings/production.py
@@ -7,6 +7,7 @@ Entry point for the production site.
 from __future__ import absolute_import, unicode_literals
 from .base import *
 
+
 # static and media root
 STATIC_ROOT = os.path.join(BASE_DIR, 'htdocs', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'htdocs', 'media')
--- mentoki/settings/staging.py
+++ mentoki/settings/staging.py
@@ -14,3 +14,19 @@ SSLIFY_DISABLE = True
 # static and media root
 STATIC_ROOT = os.path.join(BASE_DIR, 'htdocs', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'htdocs', 'media')
+
+# TODO: deal with SSLIFY in staging environment and remove overwrite of middelware
+
+# middleware
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'maintenancemode.middleware.MaintenanceModeMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
