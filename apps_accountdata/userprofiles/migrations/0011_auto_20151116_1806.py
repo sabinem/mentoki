05008fb8406c358f9a130632b20def663cd390f6
Sabine Maennel <sabine.maennel@gmail.com>
2015-11-20 06:04:56 +0100

    added migration

---
+++ apps_accountdata/userprofiles/migrations/0011_auto_20151116_1806.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('userprofiles', '0010_auto_20151115_1845'),
+    ]
+
+    operations = [
+    ]
