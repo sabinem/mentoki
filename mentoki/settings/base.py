0ad2a6ae8c9e1992b40af0912349a2ca558ce719
Sabine Maennel <sabine.maennel@gmail.com>
2015-11-20 07:07:16 +0100

    template bug fixed

--- mentoki/settings/base.py
+++ mentoki/settings/base.py
@@ -182,24 +182,24 @@ STATICFILES_FINDERS = (
 
 
 # templates
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+#TEMPLATES = [
+#    {
+#        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#        'DIRS': ['templates'],
+#        'APP_DIRS': True,
+#        'OPTIONS': {
+#            'context_processors': [
+#                'django.contrib.auth.context_processors.auth',
+#                'django.template.context_processors.debug',
+#                'django.template.context_processors.i18n',
+#                'django.template.context_processors.media',
+#                'django.template.context_processors.static',
+#                'django.template.context_processors.tz',
+#                'django.contrib.messages.context_processors.messages',
+#            ],
+#        },
+#    },
+#]
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
