88802786c55fc84499a6d6593fd3e2ba5a407501
Sabine Maennel <sabine.maennel@gmail.com>
2015-11-24 12:42:33 +0100

    staging settings file added

---
+++ mentoki/settings/staging.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+"""
+Entry point for the production site.
+"""
+
+from __future__ import absolute_import, unicode_literals
+from .base import *
+
+# sentry
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
+
+# static and media root
+STATIC_ROOT = os.path.join(BASE_DIR_PROJECT, 'htdocs', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR_PROJECT, 'htdocs', 'media')
+
+
+# ssl settings
+#SSLIFY_DISABLE = True
