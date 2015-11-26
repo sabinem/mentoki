93e1f392381abf919de712e02acb5194190e16a6
Sabine Maennel <sabine.maennel@gmail.com>
2015-11-26 08:02:35 +0100

    seo

--- apps_public/contact/urls.py
+++ apps_public/contact/urls.py
@@ -3,11 +3,20 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
+from django.http import HttpResponsePermanentRedirect
+from django.core.urlresolvers import reverse
 
 from .views import  ContactView, AnswerView
+from django.shortcuts import redirect
 
+def contact_redirect(request):
+    print 'in redirect'
+    return HttpResponsePermanentRedirect(reverse('contact:contact'))
 
 urlpatterns = patterns("",
+
+    url(r'^kontakt$', contact_redirect,
+        ),
 
     url(r'^$', ContactView.as_view(),
         {'template':'contact/pages/contact.html'}, name='contact'),
@@ -16,3 +25,4 @@ urlpatterns = patterns("",
          {'template':'contact/pages/answer.html'}, name='answer'),
 
 )
+
