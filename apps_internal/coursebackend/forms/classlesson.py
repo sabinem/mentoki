a4110cccd3a5abcccf730948f3a737df8bcb0ccf
Sabine Maennel <sabine.maennel@gmail.com>
2016-01-20 15:50:34 +0100

    Feld vorübergehend aus den Forms entfernt

--- apps_internal/coursebackend/forms/classlesson.py
+++ apps_internal/coursebackend/forms/classlesson.py
@@ -21,7 +21,7 @@ class ClassLessonBlockForm(forms.ModelForm):
 
     class Meta:
         model = ClassLesson
-        fields = ('nr', 'title', 'description', 'text', 'show_number' )
+        fields = ('nr', 'title', 'description', 'text' )
 
 
 class ClassLessonForm(forms.ModelForm):
@@ -29,7 +29,7 @@ class ClassLessonForm(forms.ModelForm):
 
     class Meta:
         model = ClassLesson
-        fields = ('parent', 'nr', 'show_number', 'title', 'description', 'text' )
+        fields = ('parent', 'nr', 'title', 'description', 'text' )
 
     def __init__(self, *args, **kwargs):
 
@@ -49,7 +49,7 @@ class ClassLessonStepForm(forms.ModelForm):
 
     class Meta:
         model = ClassLesson
-        fields = ('nr', 'show_number', 'title', 'parent', 'description', 'text', 'material', 'is_homework',
+        fields = ('nr',  'title', 'parent', 'description', 'text', 'material', 'is_homework',
                   'show_work_area', 'due_date', 'extra_text' )
 
     def __init__(self, *args, **kwargs):
